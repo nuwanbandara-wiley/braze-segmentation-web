227d2671f81b13301462654dd0a0a5c7f94c9d73
nuwanbandara-wiley <nbandara@wiley.com>
2021-04-23 19:05:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+const appboy = require("@braze/web-sdk");
+const USER_ID = "ae143444-0e26-4eda-8bfa-4dcec82b2250";
+const SESSION_ID = Math.random().toString().replace(".", "");
 
 function App() {
+  window.appboy = appboy;
+  console.log("SESSION_ID", SESSION_ID);
+  const [isSending, setIsSending] = useState(false);
+  useEffect(() => {
+    const initializeAppboy = () => {
+      appboy.initialize("3d0e6267-6e58-4da3-b52b-a4996f9ffbcb", {
+        baseUrl: "https://sdk.iad-03.appboy.com/api/v3",
+        enabbleLoggin: true,
+        minimumIntervalBetweenTriggerActionsInSeconds: 1,
+      });
+
+      appboy.openSession();
+      appboy.changeUser(SESSION_ID);
+
+      appboy.subscribeToInAppMessage((inAppMessage) => {
+        appboy.display.showInAppMessage(inAppMessage);
+      });
+    };
+
+    const subscribeToSegmentationController = () => {
+      const url = `https://poc-segmentation-api.herokuapp.com/api/v1/segmentation-controller/subscribe?sessionId=${SESSION_ID}`;
+      const eventSource = new EventSource(url);
+
+      eventSource.addEventListener("onUpdate", (e) => {
+        const data = JSON.parse(e.data);
+        console.log(new Date(), data);
+        appboy.logCustomEvent(data.eventName);
+      });
+
+      eventSource.addEventListener("error", (e) => {
+        /*if(e.currentTarget.readyState !== EventSource.CLOSED) {
+          eventSource.close();
+          console.error('Error onUpdate event stream');
+        }*/
+      });
+    };
+
+    initializeAppboy();
+    subscribeToSegmentationController();
+  }, []);
+
+  const onClickTriggerEventButton = (e) => {
+    setIsSending(true);
+    fetch(
+      `https://poc-segmentation-api.herokuapp.com/api/v1/segmentation-controller?sessionId=${SESSION_ID}`,
+      {
+        method: "POST",
+        cache: "no-cache",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          sessionId: SESSION_ID,
+          eventName: "Load_Dashboard",
+          payload: JSON.stringify({
+            userId: USER_ID,
+            time: new Date(),
+          }),
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setIsSending(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button type="button" onClick={onClickTriggerEventButton}>
+        {isSending ? "Sending Event..." : "Trigger Event"}
+      </button>
     </div>
   );
 }
